077cfd352b9568571298ec274ef4b0673fb8aeeb
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-07 20:50:03 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,59 +1,3 @@
-# import smtplib
-#
-#
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.starttls()
-# server.login('2027195ayush@gmail.com', 'rbhrvbotytxtdiwy')
-#
-# message = "Hey there, Sending mail through Python!"
-# server.sendmail('2027195ayush@gmail.com', 'surajten00@gmail.com', message)
-#
-# server.quit()
-
-# ------------------------OR------------------------
-# import smtplib
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.base import MIMEBase
-# from email import encoders
-#
-# from_add = '2027195ayush@gmail.com'
-# to_add = 'ayushkr9976@gmail.com'
-# subject = "Mail from Python Script - 1"
-#
-# msg = MIMEMultipart()
-# msg['From'] = from_add
-# msg['To'] = to_add
-# msg['Subject'] = subject
-#
-# # --------------<b> TAG OR BOLD TAG------AND <br> FOR NEW LINE----------
-# body1 = "<b>Hey there! Sending mail through Python!</b><br>Good Morning"
-# # body2 = "Good morning"
-# msg.attach(MIMEText(body1, 'html'))
-# # msg.attach(MIMEText(body2, 'html'))
-#
-# my_file = open("scrape.csv", 'rb')
-#
-# part = MIMEBase('application', 'octet-stream')
-# part.set_payload((my_file).read())
-# encoders.encode_base64(part)
-# part.add_header('content-Disposition', 'attachment; filename= ' + 'scrape.csv')
-# msg.attach(part)
-#
-#
-# message = msg.as_string()
-#
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.starttls()
-# server.login('2027195ayush@gmail.com', 'rbhrvbotytxtdiwy')
-#
-# # message = "Hey there, Sending mail through Python!"
-# server.sendmail(from_add, to_add, message)
-#
-# server.quit()
-
-# ------------------------------------------------------THE SEND_MAIL.PY IS INTEGRATED BY OUR SCRAPE.PY----------------------------------------------------------------------------------
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -61,20 +5,17 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 def send(filename):
-    from_add = '2027195ayush@gmail.com'
-    to_add = 'neelampanjwani72@gmail.com'
+    from_add = 'ENTER YOUR E-MAIL ADDRESS'
+    to_add = 'TO E-MAIL ADDRESS'
     subject = "Finance Stock Report"
 
     msg = MIMEMultipart()
     msg['From'] = from_add
     msg['To'] = to_add
     msg['Subject'] = subject
-
-# --------------<b> TAG OR BOLD TAG------AND <br> FOR NEW LINE----------
+    
     body1 = "<b>Today's Finance Report Attached</b><br>Good Morning"
-# body2 = "Good morning"
     msg.attach(MIMEText(body1, 'html'))
-# msg.attach(MIMEText(body2, 'html'))
 
     my_file = open(filename, 'rb')
 
@@ -89,7 +30,7 @@ def send(filename):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login('2027195ayush@gmail.com', 'rbhrvbotytxtdiwy')
+    server.login('YOUR E-MAIL ID', 'E-MAIL PASSWORD/ GOOGLE TEMP. PASWSORD')
 
     server.sendmail(from_add, to_add, message)
 
